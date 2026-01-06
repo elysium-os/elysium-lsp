5bca2505cebb1c28d9beae01879031ae250078fc
WuX <wux@thenest.dev>
2026-01-06 18:27:02 +0200

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs =
+        {
+            nixpkgs,
+            flake-utils,
+            ...
+        }:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+            in
+            {
+                devShells.default = pkgs.mkShell {
+                    shellHook = "export NIX_SHELL_NAME='elysium-lsp'";
+                    buildInputs = with pkgs; [
+                        rustup
+                    ];
+
+                    nativeBuildInputs = with pkgs; [
+                        libllvm
+                        rustPlatform.bindgenHook
+                    ];
+                };
+            }
+        );
+}
